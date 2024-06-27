8eefda1f6f9d0d47e70f587de9c76cc22e79da26
Allan Hipólito <allanhipolito66@gmail.com>
2024-06-27 02:48:40 -0300

    chore: Port coming from .env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(json({ limit: '5mb' }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
